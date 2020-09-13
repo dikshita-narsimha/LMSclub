f41c161bc05d47d42f4e2bb904f62e1916ab446c
Vinayagar <raji.pragasam@gmail.com>
2020-09-12 18:42:35 -0700

    Calender

---
+++ schedule.py
@@ -0,0 +1,38 @@
+
+weekday1="Monday"
+weekday2="Tuesday"
+
+period1="P1 Science"
+period2="P2 Math"
+period3="P3 P.F."
+period4="P4 Woodshop"
+period5="P5 ELA"
+period6="P6 S.S"
+
+Break="Break"
+Lunch="Lunch"
+Independentwork="Independent-work"
+
+time1 = "9:10-10:15"    
+time2 = "10:15-10:35"   
+time3 = "10:35-11:40"   
+time4 = "11:40-12:10" 
+time5 = "12:10-1:15"   
+time6 = "1:15-3:00"
+
+print ("Day\tTime\t\tSubject")
+print (weekday1+"\t"+time1+period1)
+print ("\t\t"+time2+Break)
+print ("\t\t"+time3+period2)
+print ("\t\t"+time4+Lunch)
+print ("\t\t"+time5+period3)
+print ("\t\t"+time6+Independentwork)
+
+print (weekday2+"\t"+time1+period4)
+print ("\t\t"+time2+Break)
+print ("\t\t"+time3+period5)
+print ("\t\t"+time4+Lunch)
+print ("\t\t"+time5+period6)
+print ("\t\t"+time6+Independentwork)
+
+
